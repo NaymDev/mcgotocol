9ae0b0d79692d3d6357fabc9e0805589c9c8b82e
NaymDev <125344509+naymdev@users.noreply.github.com>
2026-03-21 20:19:02 +0100

    feat: support zlib compression

--- conn.go
+++ conn.go
@@ -2,6 +2,7 @@ package mcgotocol
 
 import (
 	"bytes"
+	"compress/zlib"
 	"github.com/NaymDev/mcgotocol/codec"
 	"github.com/NaymDev/mcgotocol/proto"
 	"github.com/NaymDev/mcgotocol/state"
@@ -9,9 +10,12 @@ import (
 )
 
 type Connection struct {
-	writer         io.Writer
-	reader         io.Reader
+	writer io.Writer
+	reader io.Reader
+
 	packetRegistry *state.PacketRegistry
+
+	compressionThreshold int
 }
 
 func NewConnection(reader io.Reader, writer io.Writer, packetRegistry *state.PacketRegistry) *Connection {
@@ -33,37 +37,84 @@ func (c *Connection) ReadPacket() (proto.Packet, error) {
 	}
 
 	buf := make([]byte, length)
-	_, err = io.ReadFull(c.reader, buf)
-	if err != nil {
+	if _, err := io.ReadFull(c.reader, buf); err != nil {
 		return nil, err
 	}
 
 	br := bytes.NewReader(buf)
 
-	packetID, err := codec.ReadVarInt(br)
+	var dataReader io.Reader = br
+
+	if c.compressionThreshold >= 0 {
+		dataLength, err := codec.ReadVarInt(br)
+		if err != nil {
+			return nil, err
+		}
+
+		if dataLength != 0 {
+			zr, err := zlib.NewReader(br)
+			if err != nil {
+				return nil, err
+			}
+			defer zr.Close()
+
+			decompressed, err := io.ReadAll(zr)
+			if err != nil {
+				return nil, err
+			}
+
+			dataReader = bytes.NewReader(decompressed)
+		}
+	}
+
+	packetID, err := codec.ReadVarInt(dataReader)
 	if err != nil {
 		return nil, err
 	}
 
-	return c.packetRegistry.Decode(int32(packetID), br)
+	return c.packetRegistry.Decode(int32(packetID), dataReader)
 }
 
 func (c *Connection) WritePacket(p proto.Packet) error {
-	buf := &bytes.Buffer{}
+	var buf bytes.Buffer
 
-	if err := codec.WriteVarInt(buf, codec.VarInt(p.ID())); err != nil {
+	if err := codec.WriteVarInt(&buf, codec.VarInt(p.ID())); err != nil {
 		return err
 	}
 
-	if err := p.Encode(buf); err != nil {
+	if err := p.Encode(&buf); err != nil {
 		return err
 	}
 
 	packetData := buf.Bytes()
-	if err := codec.WriteVarInt(c.writer, codec.VarInt(len(packetData))); err != nil {
+
+	var dataBuf bytes.Buffer
+
+	if c.compressionThreshold >= 0 {
+		if len(packetData) >= c.compressionThreshold {
+			if err := codec.WriteVarInt(&dataBuf, codec.VarInt(len(packetData))); err != nil {
+				return err
+			}
+
+			zw := zlib.NewWriter(&dataBuf)
+			if _, err := zw.Write(packetData); err != nil {
+				return err
+			}
+			zw.Close()
+		} else {
+			if err := codec.WriteVarInt(&dataBuf, 0); err != nil {
+				return err
+			}
+			dataBuf.Write(packetData)
+		}
+	} else {
+		dataBuf.Write(packetData)
+	}
+
+	if err := codec.WriteVarInt(c.writer, codec.VarInt(dataBuf.Len())); err != nil {
 		return err
 	}
 
-	_, err := c.writer.Write(packetData)
+	_, err := c.writer.Write(dataBuf.Bytes())
 	return err
 }
