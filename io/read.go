be97368bbeadc29765fa7c7f03275f86b1c90709
NaymDev <125344509+naymdev@users.noreply.github.com>
2026-04-08 20:42:10 +0200

    feat: packet read and write method

---
+++ io/read.go
@@ -0,0 +1,31 @@
+package io
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/NaymDev/mcgotocol/codec"
+	"github.com/NaymDev/mcgotocol/proto"
+	"github.com/NaymDev/mcgotocol/state"
+)
+
+func ReadPacket(r io.Reader, registry *state.PacketRegistry) (proto.Packet, error) {
+	length, err := codec.ReadVarInt(r)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := make([]byte, length)
+	if _, err := io.ReadFull(r, buf); err != nil {
+		return nil, err
+	}
+
+	br := bytes.NewReader(buf)
+
+	packetID, err := codec.ReadVarInt(br)
+	if err != nil {
+		return nil, err
+	}
+
+	return registry.Decode(int32(packetID), br)
+}
---
+++ io/write.go
@@ -0,0 +1,31 @@
+package io
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/NaymDev/mcgotocol/codec"
+	"github.com/NaymDev/mcgotocol/proto"
+)
+
+func WritePacket(w io.Writer, p proto.Packet) error {
+	var buf bytes.Buffer
+
+	if err := p.Encode(&buf); err != nil {
+		return err
+	}
+
+	if err := codec.WriteVarInt(&buf, codec.VarInt(p.ID())); err != nil {
+		return err
+	}
+
+	data := buf.Bytes()
+	length := len(data)
+
+	if err := codec.WriteVarInt(w, codec.VarInt(length)); err != nil {
+		return err
+	}
+
+	_, err := w.Write(data)
+	return err
+}
