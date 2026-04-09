08bec31bb23f3608354a9273dff6e8b361bb4019
NaymDev <125344509+naymdev@users.noreply.github.com>
2026-04-09 12:56:41 +0200

    feat: added ClientEncryptionRequest and ServerEncryptionResponse packet

--- packet/login.go
+++ packet/login.go
@@ -1,9 +1,10 @@
 package packet
 
 import (
+	"io"
+
 	"github.com/NaymDev/mcgotocol/codec"
 	"github.com/NaymDev/mcgotocol/proto"
-	"io"
 )
 
 type ServerLoginStart struct {
@@ -53,6 +54,77 @@ func (c *ClientLoginSuccess) Decode(reader io.Reader) error {
 		return err
 	}
 	if c.Username, err = codec.ReadString(reader); err != nil {
+		return err
+	}
+	return nil
+}
+
+type ClientEncryptionRequest struct {
+	ServerID    string
+	PublicKey   []byte
+	VerifyToken []byte
+}
+
+var _ proto.Packet = (*ClientEncryptionRequest)(nil)
+
+func (c *ClientEncryptionRequest) ID() int32 {
+	return 0x01
+}
+
+func (c *ClientEncryptionRequest) Encode(writer io.Writer) error {
+	if err := codec.WriteString(writer, c.ServerID); err != nil {
+		return err
+	}
+	if err := codec.WriteByteArray(writer, c.PublicKey); err != nil {
+		return err
+	}
+	if err := codec.WriteByteArray(writer, c.VerifyToken); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *ClientEncryptionRequest) Decode(reader io.Reader) error {
+	var err error
+	if c.ServerID, err = codec.ReadString(reader); err != nil {
+		return err
+	}
+	if c.PublicKey, err = codec.ReadByteArray(reader); err != nil {
+		return err
+	}
+	if c.VerifyToken, err = codec.ReadByteArray(reader); err != nil {
+		return err
+	}
+	return nil
+}
+
+type ServerEncryptionResponse struct {
+	SharedSecret []byte
+	VerifyToken  []byte
+}
+
+var _ proto.Packet = (*ServerEncryptionResponse)(nil)
+
+func (s *ServerEncryptionResponse) ID() int32 {
+	return 0x01
+}
+
+func (s *ServerEncryptionResponse) Encode(writer io.Writer) error {
+	if err := codec.WriteByteArray(writer, s.SharedSecret); err != nil {
+		return err
+	}
+	if err := codec.WriteByteArray(writer, s.VerifyToken); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *ServerEncryptionResponse) Decode(reader io.Reader) error {
+	var err error
+	if s.SharedSecret, err = codec.ReadByteArray(reader); err != nil {
+		return err
+	}
+	if s.VerifyToken, err = codec.ReadByteArray(reader); err != nil {
 		return err
 	}
 	return nil
