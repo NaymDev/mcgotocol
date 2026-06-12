97b6fb604ae174ea9cf6a3ae59121e104cc8dd47
NaymDev <125344509+naymdev@users.noreply.github.com>
2026-06-12 20:48:00 +0200

    feat: packet write test

--- codec/encode_test.go
+++ codec/encode_test.go
@@ -5,6 +5,12 @@ import (
 	"io"
 	"math"
 	"testing"
+
+	mcio "github.com/NaymDev/mcgotocol/io"
+	"github.com/NaymDev/mcgotocol/packet"
+	"github.com/NaymDev/mcgotocol/profile"
+	"github.com/NaymDev/mcgotocol/proto"
+	"github.com/google/uuid"
 )
 
 func BenchmarkVarIntWrite(b *testing.B) {
@@ -170,6 +176,203 @@ func BenchmarkDoubleWrite(b *testing.B) {
 	}
 
 	benchEncoder(b, WriteDouble, cases)
+}
+
+func BenchmarkPacketsWrite(b *testing.B) {
+	cases := []struct {
+		name  string
+		value proto.Packet
+	}{
+		{
+			name: "handshake status",
+			value: &packet.ServerHandshake{
+				ProtocolVersion: 760,
+				ServerAddress:   "localhost",
+				ServerPort:      25565,
+				NextState:       packet.StatusHandshakeIntent,
+			},
+		},
+		{
+			name: "handshake login",
+			value: &packet.ServerHandshake{
+				ProtocolVersion: 761,
+				ServerAddress:   "mc.example.com",
+				ServerPort:      25565,
+				NextState:       packet.LoginHandshakeIntent,
+			},
+		},
+		{
+			name: "login start",
+			value: &packet.ServerLoginStart{
+				Name: "Steve",
+			},
+		},
+		{
+			name: "encryption response",
+			value: &packet.ServerEncryptionResponse{
+				SharedSecret: []byte{0x01, 0x02, 0x03},
+				VerifyToken:  []byte{0x0A, 0x0B},
+			},
+		},
+		{
+			name: "disconnect login",
+			value: &packet.ClientDisconnect{
+				Reason: `{"text":"Disconnected"}`,
+			},
+		},
+		{
+			name: "login success",
+			value: &packet.ClientLoginSuccess{
+				UUID:     "123e4567-e89b-12d3-a456-426614174000",
+				Username: "Steve",
+			},
+		},
+		{
+			name: "set compression",
+			value: &packet.ClientSetCompression{
+				Threshold: 256,
+			},
+		},
+		{
+			name:  "status request",
+			value: &packet.ServerStatusRequest{},
+		},
+		{
+			name: "status response",
+			value: &packet.ClientStatusResponse{
+				JSONResponse: `{"version":{"name":"1.20.4","protocol":765},"players":{"max":100,"online":5},"description":{"text":"Hello world"}}`,
+			},
+		},
+		{
+			name: "status ping",
+			value: &packet.ServerStatusPing{
+				Payload: 9223372036854775807,
+			},
+		},
+		{
+			name: "status pong",
+			value: &packet.ClientStatusPong{
+				Payload: 42,
+			},
+		},
+		{
+			name: "keep alive server",
+			value: &packet.ServerKeepAlive{
+				KeepAliveID: 12345,
+			},
+		},
+		{
+			name: "keep alive client",
+			value: &packet.ClientKeepAlive{
+				KeepAliveID: 54321,
+			},
+		},
+		{
+			name: "join game",
+			value: &packet.ClientJoinGame{
+				EntityID:         1,
+				Gamemode:         packet.GamemodeCreative,
+				Dimension:        0,
+				Difficulty:       1,
+				MaxPlayers:       20,
+				LevelType:        "default",
+				ReducedDebugInfo: false,
+			},
+		},
+		{
+			name: "chat message",
+			value: &packet.ClientChatMessage{
+				JSONData: `{"text":"Hello world"}`,
+				Position: 1,
+			},
+		},
+		{
+			name: "set spawn position",
+			value: &packet.ClientSetSpawnPosition{
+				X: 100,
+				Y: 64,
+				Z: -100,
+			},
+		},
+		{
+			name: "player position and look",
+			value: &packet.ClientPlayerPositionAndLook{
+				X:     10.5,
+				Y:     64.0,
+				Z:     -30.25,
+				Yaw:   90.0,
+				Pitch: 45.0,
+				Flags: uint8(packet.X | packet.Y),
+			},
+		},
+		{
+			name: "spawn player",
+			value: &packet.ClientSpawnPlayer{
+				EntityID:    1,
+				PlayerUUID:  uuid.MustParse("123e4567-e89b-12d3-a456-426614174000"),
+				X:           100,
+				Y:           64,
+				Z:           100,
+				Yaw:         0,
+				Pitch:       0,
+				CurrentItem: 0,
+				Metadata:    nil,
+			},
+		},
+		{
+			name: "player abilities",
+			value: &packet.ClientPlayerAbilities{
+				Flags:               0x01,
+				FlyingSpeed:         0.05,
+				FieldOfViewModifier: 1.0,
+			},
+		},
+		{
+			name: "player list add",
+			value: &packet.ClientPlayerListItem{
+				Action: packet.PlayerListItemActionAddPlayer,
+				Players: []packet.PlayerListItemEntry{
+					{
+						UUID: uuid.MustParse("123e4567-e89b-12d3-a456-426614174000"),
+						Name: func() *string { s := "Steve"; return &s }(),
+						Properties: []packet.Property{
+							packet.FromProfileProperty(profile.Property{
+								Name:  "skin",
+								Value: "default",
+							}),
+						},
+						Gamemode: func() *VarInt { v := VarInt(packet.GamemodeCreative); return &v }(),
+						Ping:     func() *VarInt { v := VarInt(50); return &v }(),
+					},
+				},
+			},
+		},
+		{
+			name: "player list update gamemode",
+			value: &packet.ClientPlayerListItem{
+				Action: packet.PlayerListItemActionUpdateGamemode,
+				Players: []packet.PlayerListItemEntry{
+					{
+						UUID:     uuid.MustParse("123e4567-e89b-12d3-a456-426614174000"),
+						Gamemode: func() *VarInt { v := VarInt(packet.GamemodeSurvival); return &v }(),
+					},
+				},
+			},
+		},
+		{
+			name: "player list remove",
+			value: &packet.ClientPlayerListItem{
+				Action: packet.PlayerListItemActionRemovePlayer,
+				Players: []packet.PlayerListItemEntry{
+					{
+						UUID: uuid.MustParse("123e4567-e89b-12d3-a456-426614174000"),
+					},
+				},
+			},
+		},
+	}
+
+	benchEncoder(b, mcio.WritePacket, cases)
 }
 
 func benchEncoder[T any](
